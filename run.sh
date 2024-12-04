7f1b9f32bc022a9c715de70a811a4c652ad6b311
Muhammad Jawad <jawad2k01@gmail.com>
2024-12-04 14:46:12 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=simulated-job
 #SBATCH --output=simulated-job.out
-#SBATCH --time=00:05:00  # Set the job time limit to 5 minutes
+#SBATCH --time=00:10:00  # Set the job time limit to 10 minutes
 #SBATCH --partition=debug
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
