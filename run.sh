5aaf8c8fa89ec42e1aeb228604fce8d7bcd45bfe
Muhammad Jawad <jawad2k01@gmail.com>
2024-12-04 14:48:34 +0500

    30 minutes

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=simulated-job
 #SBATCH --output=simulated-job.out
-#SBATCH --time=00:10:00  # Set the job time limit to 10 minutes
+#SBATCH --time=00:30:00  # Set the job time limit to 30 minutes
 #SBATCH --partition=debug
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
-# Simulate some processing by sleeping for 5 minutes
+# Simulate some processing by sleeping 
 echo "Job started at $(date)"
-sleep 900  # Sleep for 900 seconds (10 minutes)
+sleep 1800  # Sleep 30 minutes
 echo "Job finished at $(date)"
